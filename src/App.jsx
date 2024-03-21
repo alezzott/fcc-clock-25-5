bbd75409715c08d1bdf9ddc9828d91410177fe7b
Alex Faustino <faustino.alex@outlook.com>
2024-03-21 18:30:29 -0300

    ref: "add footer by"

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import { Clock } from './components/Clock';
 
 function App() {
     return (
-        <section>
-            <h1 className="text-main font-semibold text-[72px] text-center">Clock.js</h1>
-            <Clock />
-        </section>
+        <>
+            <section>
+                <h1 className="text-main font-semibold text-[72px] text-center">Clock.js</h1>
+                <Clock />
+            </section>
+            <h1 className="font-semibold pt-3 text-center">
+                By
+                <a href="https://github.com/alezzott" className="hover:text-lime-500 " target="_blank">
+                    {' '}
+                    alezzo 👨‍💻
+                </a>
+            </h1>
+        </>
     );
 }
 
