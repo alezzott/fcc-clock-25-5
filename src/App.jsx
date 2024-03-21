7bda162a302cc7e422a36e63749796fab602f142
Alex Faustino <faustino.alex@outlook.com>
2024-03-21 18:25:03 -0300

    ref:  "add main title"'

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Clock } from './components/Clock';
 function App() {
     return (
         <section>
-            <h1>Clock.js</h1>
+            <h1 className="text-main font-semibold text-[72px] text-center">Clock.js</h1>
             <Clock />
         </section>
     );
