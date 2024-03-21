af2807a573e7c670aadb71461681c13d7586da1e
Alex Faustino <faustino.alex@outlook.com>
2024-03-21 18:25:55 -0300

    ref : "update time setting styles"

--- src/components/TimeSetting.jsx
+++ src/components/TimeSetting.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { RxDoubleArrowDown, RxDoubleArrowUp } from 'react-icons/rx';
 
 export const TimerControl = ({ id, label, value, onIncrement, onDecrement, sessionType }) => {
     const handleDecrement = () => {
@@ -11,24 +12,26 @@ export const TimerControl = ({ id, label, value, onIncrement, onDecrement, sessi
 
     return (
         <section className="mt-10">
-            <h1 id={`${id}-label`}>{label}</h1>
-            <article className="gap-6 flex justify-center items-center mt-5">
+            <h1 id={`${id}-label`} className="font-mono text-2xl">
+                {label}
+            </h1>
+            <article className=" mt-5 gap-5 flex justify-center items-center ">
                 <button
                     id={`${sessionType}-decrement`}
-                    className="p-5 bg-slate-500 rounded-lg text-2xl"
+                    className="p-5 bg-[#7c7c7c] rounded-lg  text-2xl"
                     onClick={handleDecrement}
                 >
-                    -
+                    <RxDoubleArrowDown size={35} />
                 </button>
-                <h2 id={`${id}-length`} className="text-2xl w-auto">
+                <h2 id={`${id}-length`} className=" font-mono text-2xl w-auto px-3">
                     {value}
                 </h2>
                 <button
                     id={`${sessionType}-increment`}
-                    className="p-5 bg-slate-500 rounded-lg text-2xl"
+                    className="p-5 bg-[#7c7c7c] rounded-lg text-2xl"
                     onClick={handleIncrement}
                 >
-                    +
+                    <RxDoubleArrowUp size={35} />
                 </button>
             </article>
         </section>
